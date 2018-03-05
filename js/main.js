4b072ec2a380aa74467dec2e30f1a6c8124db819
Carrie Smith <carriesmith59@gmail.com>
2018-03-04 22:50:58 -0500

    worked on portfolio page

---
+++ js/main.js
@@ -0,0 +1,10 @@
+console.log('working');
+
+$(document).ready(function(){
+  console.log('document is ready');
+
+$('#btn1').click(function(){
+  console.log('button works');
+})
+
+}) // closes document.ready
