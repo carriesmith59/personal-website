f7fbdf0e4779444a0c8f9e7c3b725b937d7cb685
Carrie Smith <carriesmith59@gmail.com>
2018-03-06 01:48:42 -0500

    fixed portfolio page

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ console.log('working');
 $(document).ready(function(){
   console.log('document is ready');
 
-$('#btn1').click(function(){
-  console.log('button works');
-})
+
 
 }) // closes document.ready
